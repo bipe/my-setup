3af5aea11ee1cde20b0d38b92f12f7f3125068a3
bipe <itsmemarconi@gmail.com>
2025-05-24 19:22:00 -0300

    fix git commands

--- setup.sh
+++ setup.sh
@@ -52,8 +52,8 @@ defaults write com.apple.controlcenter "BatteryShowPercentage" -bool true && kil
 defaults write com.apple.finder ShowPathbar -bool true && killall Finder
 
 # Config Git
-git config --global user.email “itsmemarconi@gmail.com”       
-git config --global user.name "bipe"         
+git config --global user.email itsmemarconi@gmail.com
+git config --global user.name bipe
 
 
 # Envs
